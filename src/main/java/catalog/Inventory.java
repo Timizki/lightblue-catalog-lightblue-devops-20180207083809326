e87b1cd874cc61143e973ca22b9b70b669a498b3
Timo Timo Paananen <timo.paananen@kela.fi>
2018-02-08 09:50:19 +0000

    - inventory added

---
+++ src/main/java/catalog/Inventory.java
@@ -0,0 +1,57 @@
+package catalog;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="items")
+public class Inventory {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationTYpe.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String name;
+	
+	@NotNull
+	private String description;
+	
+	@NotNull
+	private int price;
+	
+	@NotNull
+	private String img_alt;
+	
+	@NotNull
+	private String img;
+	
+	@NotNul
+	private int stock;
+	
+	public Inventory(long id) {
+		this.id = id;
+	}
+	
+	public Inventory(String name, String desc, int price, String alt, String img, int stock) {
+		this.name = name;
+		this.description = desc;
+		this.price = price;
+		this.img_alt = alt;
+		this.img = img;
+		this.stock = stock;
+	}
+
+	public Inventory() {
+		
+	}	
+	
+	public String toString() {
+		return "name="+name+", id="+this.id;
+	}
+	
+}
